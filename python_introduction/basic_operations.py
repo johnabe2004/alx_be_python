0c7527204185e8a429987bef666b619c082cd71d
Yohannes <johnabe2004@gmail.com>
2024-09-15 11:43:21 -0700

    basic paython oprations created

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+#basic_operations
+number1=5
+number2=10
+sum=number1+number2
+subtraction=number1-number2
+multiplication= number1*number2 
+print("Subtraction of",number1,"-",number2,"=",subtraction)
+print("Multiplication of",number1,"*",number2,"=",multiplication)
+print("Sum of",number1,"+",number2,"=",sum)
