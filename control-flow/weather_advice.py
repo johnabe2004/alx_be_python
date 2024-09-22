a10b593a64450f9a3e5155eb96a65f3f26227eaa
Yohannes <johnabe2004@gmail.com>
2024-09-22 11:08:29 -0700

    Weather advice updated

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:control-flow/weather_advice.py
-def weather_advice():
-    weather = input("What's the weather like today? (sunny, rainy, cold): ").lower()
-=======
-weather = input("What's the weather like today? (sunny, rainy, cold): ").lower()
->>>>>>> 848c64d29711230b75a75b4ffc7274bf40463a6d:weather_advice.py
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
     # Provide clothing recommendations based on the weather input
     if weather == "sunny":
         print("Wear a t-shirt and sunglasses.")
@@ -13,4 +8,3 @@ weather = input("What's the weather like today? (sunny, rainy, cold): ").lower()
         print("Make sure to wear a warm coat and a scarf.")
     else:
         print("Sorry, I don't have recommendations for this weather.")
-weather_advice()
