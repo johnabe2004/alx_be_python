88f95e8bf5a5aabc6920651483ebb8fc0139c108
Yohannes <johnabe2004@gmail.com>
2024-09-22 12:21:19 -0700

    pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,8 @@
+size = int(input("Enter the size of the pattern: "))
+if size <= 0: 
+     print("Please enter a positive integer greater than zero.")
+row = 0
+while row < size:
+    for col in range(size): print("*", end=" ")
+    print() 
+    row += 1
