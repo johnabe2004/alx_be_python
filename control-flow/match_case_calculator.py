4abe643fd2ef709c938b6cbe713c37dbb3e4b7b0
Yohannes <johnabe2004@gmail.com>
2024-09-27 13:07:53 -0700

    match case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,27 +1,27 @@
-        num1 = int(input("Enter the first number: "))
-        num2 = int(input("Enter the second number: "))
-    # Prompt the user to select an operation
-    print("Select an operation: ")
-    print("1. Addition (+)")
-    print("2. Subtraction (-)")
-    print("3. Multiplication (*)")
-    print("4. Division (/)")
-    operation = input("Enter the operation symbol (+, -, *, /): ")
-    match operation:
-        case "+":
-            result = num1 + num2
-            print("The result of", num1 + num2,"is:", result)
-        case "-":
-            result = num1 - num2
-            print("The result of", num1 - num2, "is:", result)
-        case "*":
-            result = num1 * num2
-            print("The result of", num1 * num2,"is:", result)
-        case "/":
-            if num2 != 0:
-                result = num1 / num2
-                print (“The result of “, num1 + num2,” is:”, result)
-            else:
-                print("Error! Division by zero is not allowed.")
-        case _:
-            print("Invalid operation selected. Please choose one of (+, -, *, /).")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+# Prompt the user to select an operation
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+operation = input("Enter the operation symbol (+, -, *, /): ")
+match operation:
+    case "+":
+        result = num1 + num2
+        print("The result of", num1 ,"+", num2,"is:", result)
+    case "-":
+        result = num1 - num2
+        print("The result of", num1 ,"-", num2, "is:", result)
+    case "*":
+        result = num1 * num2
+        print("The result of", num1, "*", num2,"is:", result)
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print ("The result of", num1," /", num2,"is:", result)
+        else:
+            print("Error! Division by zero is not allowed.")
+    case _:
+        print("Invalid operation selected. Please choose one of (+, -, *, /).")
