1c711dc6817605f9bd026aecd7bb86f2b006bf27
johnabe2004 <johnabe2004@gmail.com>
2024-10-13 13:47:15 -0700

    book_class.py created

--- oop/book_class.py
+++ oop/book_class.py
@@ -13,9 +13,20 @@ class Book:
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
 
 if __name__ == "__main__":
-    book1 = Book("1984", "George Orwell", 1949)
-    print(str(book1))
-    print(repr(book1))
-    del book1
+    main()
